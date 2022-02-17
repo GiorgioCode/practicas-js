a77d45e4e173950d71a8aec5d46a5b68b32c87c8
Jorge Angel PAEZ <jorgeangelpaez@outlook.com>
2022-02-16 23:25:50 -0300

    practica funciones

---
+++ 06_funciones/script.js
@@ -0,0 +1,8 @@
+function sumar (numero1, numero2) {
+    resultado = numero1 + numero2;
+    return resultado;
+}
+
+let sumador1 = parseInt(prompt("insertar numero1:"))
+let sumador2 = parseInt(prompt("insertar numero2:"))
+alert(sumar(sumador1, sumador2 ));
